6302d9fe6290860d726187b3bc7037d2c6bbe4cf
gustavodepaulasilva2 <99282594+gustavodepaulasilva2@users.noreply.github.com>
2023-07-19 15:32:43 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyAY2WG3AoyaWi4ObySiwbGBVmSQXEQlgk0",
+    authDomain: "espectagrama-c50ca.firebaseapp.com",
+    databaseURL: "https://espectagrama-c50ca-default-rtdb.firebaseio.com",
+    projectId: "espectagrama-c50ca",
+    storageBucket: "espectagrama-c50ca.appspot.com",
+    messagingSenderId: "454187009868",
+    appId: "1:454187009868:web:5b1b7f43ee12bc9e27f1e9"
+  };
+  
